f810d9e15752ca2832018dacda25c9fde68e5920
Matias Lazaro <mglp2404@gmail.com>
2025-04-14 22:12:17 -0500

    create front

--- backend/routes/configuracion.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const configuracionController = require('../controllers/configuracionController');
-
-router.get('/', configuracionController.getConfiguracion);
-router.put('/', configuracionController.updateConfiguracion);
-
-module.exports = router;
